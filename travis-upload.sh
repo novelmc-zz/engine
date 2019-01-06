704e122192ef2044f5cc26e47731742614c9ff03
Nathan Curran <nathan.curran10012@gmail.com>
2019-01-06 18:00:39 +1100

    it's novelengine not novelmc untuned

--- travis-upload.sh
+++ travis-upload.sh
@@ -3,7 +3,7 @@
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   export SSHPASS=${SFTP_PASSWORD}
   sshpass -e sftp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oLogLevel=ERROR -oBatchMode=no -oPort=${SFTP_PORT} -b - ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH} << !
-    put target/novelmc-*.jar novelmc.jar
+    put target/novelengine-*.jar novelengine.jar
     bye
 !
   echo "Artifact upload status: "$?
