dc1d9d2e39bbab4963ae351ccf9fc47b8498f69b
Untuned <untunedmc@gmail.com>
2019-01-08 17:51:18 -0500

    missing ], fix echo

--- travis-upload.sh
+++ travis-upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" = "master"]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" = "master" ]; then
   export SSHPASS=${SFTP_PASSWORD}
   sshpass -e sftp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oLogLevel=ERROR -oBatchMode=no -oPort=${SFTP_PORT} -b - ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH} << !
     put target/novelengine-*.jar novelengine.jar
@@ -8,5 +8,5 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" = "master"]; then
 !
   echo "Artifact upload status: "$?
 else
-  echo "Skipping artifact upload for pull request build."
+  echo "Skipping artifact upload, is PR or not on master branch."
 fi
