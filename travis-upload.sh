aed76fdf4c91d7b342dca3fc03a1e7ad546bac21
Untuned <untunedmc@gmail.com>
2019-01-12 19:25:46 -0500

    dev builds to dev server

--- travis-upload.sh
+++ travis-upload.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
   export SSHPASS=${SFTP_PASSWORD}
   sshpass -e sftp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oLogLevel=ERROR -oBatchMode=no -oPort=${SFTP_PORT} -b - ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH} << !
     put target/novelengine-*.jar novelengine.jar
     bye
 !
   echo "Artifact upload status: "$?
+else if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "devel" ]; then
+  export SSHPASS=${SFTP_PASSWORD}
+  sshpass -e sftp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oLogLevel=ERROR -oBatchMode=no -oPort=${SFTP_PORT} -b - ${SFTP_USER}@${SFTP_HOST}:${DSFTP_PATH} << !
+    put target/novelengine-*.jar noveldev.jar
+    bye
+!
+  echo "Artifact upload status: "$?
 else
-  echo "Skipping artifact upload on pull request"
+  echo "Skipping artifact upload."
 fi
