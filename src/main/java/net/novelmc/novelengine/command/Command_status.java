7907dbc1ccf8ee1d872206667b72d36e09d22058
serrif <45945003+serrif@users.noreply.github.com>
2019-03-02 19:53:41 -0500

    Formatting for /status

--- src/main/java/net/novelmc/novelengine/command/Command_status.java
+++ src/main/java/net/novelmc/novelengine/command/Command_status.java
@@ -10,17 +10,17 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-@CommandParameters(description = "Check the Server Status.", source = SourceType.BOTH, rank = Rank.OP)
+@CommandParameters(description = "Check the server and world status.", source = SourceType.BOTH, rank = Rank.OP)
 public class Command_status extends CommandBase {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
-        sender.sendMessage(NUtil.colorize("&8<-> &3&lINFO&r&8 Information regarding the Server."));
-        sender.sendMessage(NUtil.colorize("&8 Server is currently running with 'online-mode=" + (Bukkit.getServer().getOnlineMode() ? "true" : "false") + "'."));
-        sender.sendMessage(NUtil.colorize("&8NovelEngine Version: " + plugin.getDescription().getVersion()));
+        sender.sendMessage(NUtil.colorize("&7Server Information&8:"));
+        sender.sendMessage(NUtil.colorize("&8<-> &7Onlinemode&8:" + (Bukkit.getServer().getOnlineMode() ? "&a&ltrue" : "&c&lfalse") + "&7."));
+        sender.sendMessage(NUtil.colorize("&8<-> &7Plugin Version&8: &9&l" + plugin.getDescription().getVersion()));
         int i = 0;
         for (World world : Bukkit.getServer().getWorlds()) {
-            sender.sendMessage(NUtil.colorize("&8" + String.format("World %d: %s - %d players.", i++, world.getName(), world.getPlayers().size())));
+            sender.sendMessage(NUtil.colorize("&7" + String.format("World %d&8: &7%s with &9&l%d&r&7 players.", i++, world.getName(), world.getPlayers().size())));
         }
         return true;
     }
